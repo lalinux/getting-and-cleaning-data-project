a4486ff82c2ae5b1672d24e68a1497febdcd9803
Eduardo Garcia <egarcia@1aauto.com>
2016-03-28 10:01:09 -0700

    run_analysis.R script completed, codebook and resulting file added

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# getting-and-cleaning-data-project
-Getting and Cleaning Data - Course Project
+# Getting and Cleaning Data - Course Project
+This is the Getting and Cleaning Data project. It contains a R script called `run_analysis.R` which produce the file `tidy.csv` containing the tidy data set described in the `CodeBook.md` file.
+
+`run_analysis.R` executes the following steps: 
+
+1. Check if the original dataset exists, if not, download it
+1. Check if the original dataset has been extracted, if not, extract it.
+1. Load the features
+1. Find only the features on the mean and standard deviation to be extracted.
+1. Label the features with descriptive names
+1. Load the datasets with only the selected measurements and bind them
+1. Merge the training and test datasets
+1. Add labels to the merged data sets
+1. Load the activities
+1. Turn activities and subjects into factors
+1. Get the average of each variable grouped by each activity and subject
+1. Store the tidy data set in the file `tidy.csv`
---
+++ run_analysis.R
@@ -0,0 +1,69 @@
+library(dplyr)
+
+filename <- "dataset.zip"
+
+## Download the dataset if it doesn't exist
+if (!file.exists(filename)){
+        download.file(
+                "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
+                filename,
+                method="curl"
+        )
+}  
+
+## Extract the content, if it hasn't been extracted yet.
+if (!file.exists("UCI HAR Dataset")) { 
+        unzip(filename) 
+}
+
+## Load the features
+features <- read.table("UCI HAR Dataset/features.txt", stringsAsFactors=F)
+
+## Select only the measurements on the mean and standard deviation 
+features_wanted <- grep(".*mean.*|.*std.*", features[,2])
+
+## Label the features with descriptive names
+features_wanted.names <- features[features_wanted,2]
+features_wanted.names <- gsub('-','.', features_wanted.names)
+features_wanted.names <- gsub('[()]','', features_wanted.names)
+
+## Load the datasets with only the selected measurements and bind them
+train <- read.table("UCI HAR Dataset/train/X_train.txt")[features_wanted]
+train_subjects <- read.table("UCI HAR Dataset/train/subject_train.txt")
+train_activities <- read.table("UCI HAR Dataset/train/y_train.txt")
+train <- cbind(train_subjects, train_activities, train)
+
+test <- read.table("UCI HAR Dataset/test/X_test.txt")[features_wanted]
+test_subjects <- read.table("UCI HAR Dataset/test/subject_test.txt")
+test_activities <- read.table("UCI HAR Dataset/test/y_test.txt")
+test <- cbind(test_subjects, test_activities, test)
+
+## Merge datasets
+data <- rbind(train, test)
+
+## Add labels
+colnames(data) <- c("subject", "activity", features_wanted.names)
+
+## Load the activities
+activities <- read.table(
+                "UCI HAR Dataset/activity_labels.txt", 
+                stringsAsFactors = F, 
+                col.names = c("id", "label")
+        )
+
+## Turn activities and subjects into factors
+data$activity <- factor(
+                data$activity, 
+                levels = activities$id, 
+                labels=activities$label
+        )
+data$subject <- factor(data$subject)
+
+## Get the average of each variable for each activity and subject
+tidy <- summarize_each(
+                group_by(data, subject, activity), 
+                funs(mean)
+        )
+
+## Persist the tidy data set
+write.csv(tidy, "tidy.csv", row.names = F)
